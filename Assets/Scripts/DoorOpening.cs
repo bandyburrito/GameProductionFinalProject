4022e00b400b14b9a08610654059b82d5cae2396
Ammar Rexhaj <167920633+bandyburrito@users.noreply.github.com>
2025-12-21 17:26:03 +0100

    Push #10

---
+++ Assets/Scripts/DoorOpening.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DoorOpening : MonoBehaviour
+{
+    public GameObject Box1;
+    public GameObject Box2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
